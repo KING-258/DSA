258d4e89905779c0bc003642e6101b84ef543aa3
King <amulyaparashar258@gmail.com>
2023-08-31 21:40:23 +0530

    DSLab

--- TOH.c
+++ TOH.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
+static int count = 0;
 void ToH(int d, char rf, char rt, char via)
 {
           if(d == 1)
           {
                     printf("%d Disk moves from Rod %c to Rod %c\n",d,rf,rt);
+                    count++;
                     return;
           }
           ToH(d-1, rf, via, rt);
           printf("%d Disk moves from Rod %c to Rod %c\n",d,rf,rt);
+          count++;
           ToH(d-1, via, rt, rf);
 }
 int main()
@@ -16,5 +19,6 @@ int main()
           printf("Number of Disks\t");
           scanf("%d",&a);
           ToH(a, 'A', 'C', 'B');
+          printf("\n Moves is %d",count);
           return 0;
 }
