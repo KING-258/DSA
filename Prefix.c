2ea301feb82b12aee1dfcf4fb0b647e4a305c1fc
King <amulyaparashar258@gmail.com>
2023-09-03 17:08:11 +0530

    King

--- Prefix.c
+++ Prefix.c
@@ -1,57 +1,58 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int s[50];
-int top=0;
+int top = 0;
 void push(int ch)
 {
 	top++;
-	s[top]=ch;
+	s[top] = ch;
 }
 int pop()
 {
 	int ch;
-	ch=s[top];
-	top=top-1;
-	return(ch);
+	ch = s[top];
+	top--;
+	return (ch);
 }
 int main()
 {
-	int a,b,c,i;
+	int a, b, c, i;
 	char prefix[50];
 	printf("\nInput\t");
 	gets(prefix);
-	for(i=strlen(prefix)-1;i>=0;i--)
+	for (i = strlen(prefix) - 1; i >= 0; i--)
 	{
-		if(prefix[i]=='+')
+		if (prefix[i] == '+')
 		{
-			c=pop()+pop();
+			c = pop() + pop();
 			push(c);
 		}
-		else if(prefix[i]=='-')
+		else if (prefix[i] == '-')
 		{
-			a=pop();
-			b=pop();
-			c=b-a;
+			a = pop();
+			b = pop();
+			c = b - a;
 			push(c);
 		}
-		else if(prefix[i]=='*')
-		{	a=pop();
-		b=pop();
-		c=b*a;
-		push(c);
-		}
-		else if(prefix[i]=='/')
+		else if (prefix[i] == '*')
 		{
-			a=pop();
-			b=pop();
-			c=b/a;
+			a = pop();
+			b = pop();
+			c = b * a;
+			push(c);
+		}
+		else if (prefix[i] == '/')
+		{
+			a = pop();
+			b = pop();
+			c = b / a;
 			push(c);
 		}
 		else
 		{
-			push(prefix[i]-48);
+			push(prefix[i] - 48);
 		}
 	}
-	printf("\nFinal ans = %d",pop());
+	printf("\nFinal ans = %d", pop());
 	return 0;
 }
