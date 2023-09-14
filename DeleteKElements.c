11b85f2d996afea9c1b14c93f9b46c9dac51031c
King <amulyaparashar258@gmail.com>
2023-09-14 20:25:37 +0530

    DSLab

---
+++ DeleteKElements.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Stack
+{
+          int *st;
+          int tos;
+};
+struct Stack push(struct Stack s, int x)
+{
+          s.tos += 1;
+          s.st[s.tos] = x;
+          return s;
+}
+struct Stack del(struct Stack s, int ind)
+{
+          int i;
+          for (i = ind; i < s.tos; i++)
+          {
+                    s.st[i] = s.st[i + 1];
+          }
+          s.tos -= 1;
+          return s;
+}
+int main()
+{
+          struct Stack s;
+          int n, i, j, k, count = 0;
+          printf("Enter number of elements ");
+          scanf("%d", &n);
+          s.st = (int *)malloc(n * sizeof(int));
+          printf("Input Elements\n");
+          for (i = 0; i < n; i++)
+          {
+                    scanf("%d", &s.st[i]);
+          }
+          s.tos = n - 1;
+          printf("Input K ");
+          scanf("%d", &k);
+          while (k > 0)
+          {
+                    for (i = 0; i <= s.tos - 1; i++)
+                    {
+                              if (s.st[i] < s.st[i + 1])
+                              {
+                                        s = del(s, i);
+                                        k -= 1;
+                                        break;
+                              }
+                    }
+          }
+          printf("The final array is:\n");
+          for (i = 0; i <= s.tos; i++)
+          {
+                    printf("%d ", s.st[i]);
+          }
+          return 0;
+}
--- StackPalin.c
+++ StackPalin.c
@@ -1,56 +1,41 @@
 #include<stdio.h>
-struct Stack
+#include<string.h>
+char arr[20];
+int top = -1;
+char r[20];
+int topr = -1;
+void Push(char a)
 {
-          int arr[80];
-          int top;
-};
-void Push(struct Stack *s1, int a)
-{
-          if(s1 -> top == 79)
-          {
-                    printf("Stack Overflow");
-          }
-          else{
-                    s1->arr[s1->top++] = a;
-          }
+          top++;
+          arr[top] = a;
 }
-struct Stack Reverse(struct Stack s1)
+void PushR(char a)
 {
-          struct Stack s2;
-          s2.top = s1.top;
-          for(int i=s1.top; i>=0; i--)
-          {
-                    for(int j=0; j<s2.top; j++)
-                    {
-                              s2.arr[j] = s1.arr[i];
-                    }
-          }
-          return s2;
-}
-void Display(struct Stack s1)
-{
-          printf("\nBinary Number\n");
-          for(int i=0; i<s1.top; ++i)
-          {
-                    printf("%d",s1.arr[i]);
-          }
+          topr++;
+          r[topr] = a;
 }
 int main()
 {
-          int n, rem;
-          struct Stack s1;
-          struct Stack s2;
-          s1.top = 0;
-          printf("Decimal Number : \t");
-          scanf("%d",&n);
-          while(n!=0)
+          char in[20];
+          int l,s;
+          printf("Input String ");
+          scanf("%s",&in);
+          l = strlen(in);
+          for(int i=0; i<l; i++)
           {
-                    rem = n % 2;
-                    Push(&s1, rem);
-                    n = n / 2;
+                    Push(in[i]);
           }
-          Display(s1);
-          s2 = Reverse(s1);
-          Display(s2);
+          for(int i=l-1; i>=0; i--)
+          {
+                    PushR(in[i]);
+          }
+          s = strcmp(arr, r);
+          if (s == 0)
+          {
+                    printf("Palindrome");
+          }
+          else{
+                    printf("Not a Palindrome");
+          }
           return 0;
 }
