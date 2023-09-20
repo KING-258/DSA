fd4504034b5e72192f8d6566a6cbb40810c7cefe
King <amulyaparashar258@gmail.com>
2023-09-20 22:40:24 +0530

    DSLab

--- STUINFO.c
+++ STUINFO.c
@@ -1,75 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-typedef struct {
+typedef struct
+{
     int day;
     char *month;
     int year;
 } DOB;
-typedef struct {
+typedef struct
+{
     int reg_no;
     char *name;
     char address[100];
 } STU_INFO;
-typedef struct {
-    char * college_name;
+typedef struct
+{
+    char *college_name;
     char university_name[50];
 } COLLEGE;
-typedef struct {
+typedef struct
+{
     DOB *d;
     STU_INFO info;
     COLLEGE clg;
 } STUDENT;
-void read(STUDENT *stds,int n) {
+void read(STUDENT *stds, int n)
+{
     int i;
     char temp[100];
-    for (i=0;i<n;i++) {
-        printf("\nEnter details for student number %d ",(i+1));
+    for (i = 0; i < n; i++)
+    {
+        printf("\nEnter details for student number %d ", (i + 1));
         printf("Enter the day of DOB\n");
-        scanf("%d",&((stds+i)->d->day));
+        scanf("%d", &((stds + i)->d->day));
         printf("Enter the month of DOB\n");
-        scanf("%s",temp);
-        ((stds+i)->d->month) = (char *) malloc (strlen(temp) * sizeof(char));
-        strcpy(((stds+i)->d->month),temp);
+        scanf("%s", temp);
+        ((stds + i)->d->month) = (char *)malloc(strlen(temp) * sizeof(char));
+        strcpy(((stds + i)->d->month), temp);
         printf("Enter the year of DOB\n");
-        scanf("%d",&((stds+i)->d->year));
+        scanf("%d", &((stds + i)->d->year));
         printf("Enter the registration number\n");
-        scanf("%d",&((stds+i)->info.reg_no));
+        scanf("%d", &((stds + i)->info.reg_no));
         printf("Enter your name\n");
-        scanf("%s",temp);
-        (stds+i)->info.name = (char *) malloc (strlen(temp) * sizeof(char));
-        strcpy((stds+i)->info.name,temp);
+        scanf("%s", temp);
+        (stds + i)->info.name = (char *)malloc(strlen(temp) * sizeof(char));
+        strcpy((stds + i)->info.name, temp);
         printf("Enter address\n");
-        scanf("%s",((stds+i)->info.address));
+        scanf("%s", ((stds + i)->info.address));
         printf("Enter college name\n");
-        scanf(" %s",temp);
-        (stds+i)->clg.college_name = (char *) malloc (strlen(temp) * sizeof(char));
-        strcpy((stds+i)->clg.college_name,temp);
+        scanf(" %s", temp);
+        (stds + i)->clg.college_name = (char *)malloc(strlen(temp) * sizeof(char));
+        strcpy((stds + i)->clg.college_name, temp);
         printf("Enter university name\n");
-        scanf("%s",((stds+i)->clg.university_name));
+        scanf("%s", ((stds + i)->clg.university_name));
     }
 }
-void display(STUDENT *stds,int n) {
+void display(STUDENT *stds, int n)
+{
     int i;
-    for(i=0;i<n;i++) {
-        printf("\nDetails for student number %d ",(i+1));
-        printf("The name is : %s\nThe registration number is: %d\nCollege Name: %s\nUniversity Name: %s\nThe DOB is: %d/%s/%d\nAddress: %s",(stds+i)->info.name,(stds+i)->info.reg_no,(stds+i)->clg.college_name,(stds+i)->clg.university_name,(stds+i)->d->day,(stds+i)->d->month,(stds+i)->d->year,(stds+i)->info.address);
+    for (i = 0; i < n; i++)
+    {
+        printf("\nDetails for student number %d ", (i + 1));
+        printf("The name is : %s\nThe registration number is: %d\nCollege Name: %s\nUniversity Name: %s\nThe DOB is: %d/%s/%d\nAddress: %s", (stds + i)->info.name, (stds + i)->info.reg_no, (stds + i)->clg.college_name, (stds + i)->clg.university_name, (stds + i)->d->day, (stds + i)->d->month, (stds + i)->d->year, (stds + i)->info.address);
     }
 }
-int main() {
-    
-    int n,i;
-    printf("Enter the number of students ");
-    scanf("%d",&n);
-    STUDENT *stds;
-    stds = (STUDENT *) malloc (n*sizeof(STUDENT *));
-    
-    for(i=0;i<n;i++) {
-         
-         stds[i].d = (DOB *) malloc (100* sizeof(DOB *));
+int main()
+{
 
+    int n, i;
+    printf("Enter the number of students ");
+    scanf("%d", &n);
+    STUDENT *stds;
+    stds = (STUDENT *)malloc(n * sizeof(STUDENT *));
+
+    for (i = 0; i < n; i++)
+    {
+        stds[i].d = (DOB *)malloc(100 * sizeof(DOB *));
     }
-    read(stds,n);
-    display(stds,n);
+    read(stds, n);
+    display(stds, n);
     return 0;
 }
