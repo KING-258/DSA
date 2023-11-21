1dc08fae011478f66ccf27a3ff1e27c275dfed42
KING-258 <amulyaparashar258@gmail.com>
2023-11-21 22:38:33 +0530

    DS Lab

--- StackPalin.c
+++ StackPalin.c
@@ -1,41 +1,42 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 char arr[20];
 int top = -1;
 char r[20];
 int topr = -1;
 void Push(char a)
 {
-          top++;
-          arr[top] = a;
+    top++;
+    arr[top] = a;
 }
 void PushR(char a)
 {
-          topr++;
-          r[topr] = a;
+    topr++;
+    r[topr] = a;
 }
 int main()
 {
-          char in[20];
-          int l,s;
-          printf("Input String ");
-          scanf("%s",&in);
-          l = strlen(in);
-          for(int i=0; i<l; i++)
-          {
-                    Push(in[i]);
-          }
-          for(int i=l-1; i>=0; i--)
-          {
-                    PushR(in[i]);
-          }
-          s = strcmp(arr, r);
-          if (s == 0)
-          {
-                    printf("Palindrome");
-          }
-          else{
-                    printf("Not a Palindrome");
-          }
-          return 0;
+    char in[20];
+    int l, s;
+    printf("Input String ");
+    scanf("%s", &in);
+    l = strlen(in);
+    for (int i = 0; i < l; i++)
+    {
+        Push(in[i]);
+    }
+    for (int i = l - 1; i >= 0; i--)
+    {
+        PushR(in[i]);
+    }
+    s = strcmp(arr, r);
+    if (s == 0)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not a Palindrome");
+    }
+    return 0;
 }
